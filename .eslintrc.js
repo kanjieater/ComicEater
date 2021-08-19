165d9db18ebd7144ec9ae04c6512d99af8479b90
kanjieater <radioactivegum@sbcglobal.net>
2021-08-18 23:31:19 -0500

    added jest tests and starting filtering names for series

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
+  plugins: ['jest'],
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/style',
   ],
   parserOptions: {
     ecmaVersion: 12,
